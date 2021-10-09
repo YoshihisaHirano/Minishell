3591b6d0ed854882b8c47f1f3b6b139100052f2b
Aleksandr <33102108+F9lcon@users.noreply.github.com>
2021-10-09 20:46:19 +0300

    Update parser.c

--- parsing/input_parsing/parser.c
+++ parsing/input_parsing/parser.c
@@ -20,12 +20,6 @@
 //# include "readline/include/readline/rltypedefs.h"
 # include "../../minishell.h"
 
-#define PIPE 1
-#define REDRCT_OUTPUT 2
-#define REDRCT_APPEND 3
-#define REDRCT_INPUT 4
-#define HERE_DOC 5
-
 int	validation(t_list *param_list, char **envp);
 
 void show_params(t_list *list)
@@ -57,12 +51,12 @@ void show_params(t_list *list)
 			printf("str_to_cmd: |%s|\n", param_el->str_to_cmd);
 		if (param_el->here_doc_limiter)
 			printf("here_doc_limiter: |%s|\n", param_el->here_doc_limiter);
-		printf("input_mod: %s\noutput_mode: %s\n", modes[param_el->input_mod],
-			   modes[param_el->output_mode]);
+//		printf("input_mod: %s\noutput_mode: %s\n", modes[param_el->input_mod],
+//			   modes[param_el->output_mode]);
 		if (param_el->input_file)
 			printf("input_file: |%s|\n", param_el->input_file);
-		if (param_el->output_file)
-			printf("output_file: |%s|\n", param_el->output_file);
+//		if (param_el->output_file)
+//			printf("output_file: |%s|\n", param_el->output_file);
 		tmp = tmp->next;
 	}
 }
@@ -73,17 +67,17 @@ t_list_params	*create_empty_el(void)
 
 	el = malloc(sizeof(t_list_params));
 	el->input_mod = 0;
-	el->output_mode = 0;
+//	el->output_mode = 0;
 	el->here_doc_limiter = NULL;
 	el->path_app = NULL;
 	el->cmd_arr = NULL;
 	el->input_file = NULL;
-	el->output_file = NULL;
+//	el->output_file = NULL;
 	el->fd_output = -1;
 	return (el);
 }
 
-int	set_input_mode(char **input_str, t_list_params *params_el)
+int	set_input_mode(char **input_str, t_list_params *params_el, t_mshell *shell)
 {
 	char	**param_to_set;
 
@@ -101,41 +95,44 @@ int	set_input_mode(char **input_str, t_list_params *params_el)
 		(*input_str)++;
 		params_el->input_mod = REDRCT_INPUT;
 	}
-	while (ft_isspace(**input_str))
-		(*input_str)++;
-	get_io_name(param_to_set, input_str);
+	if (get_io_name(param_to_set, input_str) == -1)
+		return (-1);
+	process_io_tokens(param_to_set, shell, params_el->input_mod);
 	return (0);
 }
 
-int	set_output_mode(char **input_str, t_list_params *params_el)
+int	set_output_mode(char **input_str, t_list_params *params_el,
+				t_mshell *shell, t_list_io_params *io_el)
 {
-	if (params_el->output_mode)
-		return (handle_token_error(input_str, **input_str));
+//	if (params_el->output_mode)
+//		return (handle_token_error(input_str, **input_str));
+	while (ft_isspace(**input_str))
+		(*input_str)++;
 	if (**input_str == '|')
 	{
-		params_el->output_mode = PIPE;
+		io_el->mode = PIPE;
 		(*input_str)++;
-		return (1);
+		return (PIPE);
 	}
-	else if (!ft_strncmp(*input_str, ">>", 2))
-		params_el->output_mode = REDRCT_APPEND;
+	if (!ft_strncmp(*input_str, ">>", 2))
+		io_el->mode = REDRCT_APPEND;
 	else
-		params_el->output_mode = REDRCT_OUTPUT;
-	if (params_el->output_mode == REDRCT_APPEND)
+		io_el->mode = REDRCT_OUTPUT;
+	if (io_el->mode == REDRCT_APPEND)
 		(*input_str) += 2;
 	else
 		(*input_str)++;
-	while (ft_isspace(**input_str))
-		(*input_str)++;
-	get_io_name(&(params_el->output_file), input_str);
+	if (get_io_name(&(io_el->file_name), input_str) == -1)
+		return (-1);
+	process_io_tokens(&(io_el->file_name), shell, io_el->mode);
 	return (0);
 }
 
-
 int	set_params_to_el(char **input_str, t_list_params *el, t_mshell *shell)
 {
-	int		i;
-	int		set_mode_status;
+	int					i;
+	int					set_mode_status;
+	t_list_io_params	*io_el;
 
 	i = 0;
 	set_mode_status = 0;
@@ -144,23 +141,28 @@ int	set_params_to_el(char **input_str, t_list_params *el, t_mshell *shell)
 		if (**input_str == '\"' || **input_str == '\'')
 			handle_quotes(input_str, el, &i);
 		else if (**input_str == '<')
-			set_mode_status = set_input_mode(input_str, el);
+			set_mode_status = set_input_mode(input_str, el, shell);
 		else if (**input_str == '|' || **input_str == '>')
-			set_mode_status = set_output_mode(input_str, el);
+		{
+			while (!set_mode_status)
+			{
+				io_el = malloc(sizeof(t_list_io_params));
+				set_mode_status = set_output_mode(input_str, el, shell, io_el);
+				ft_lstadd_back(&(el->output), ft_lstnew(io_el));
+			}
+		}
 		else
 		{
 			el->str_to_cmd[i++] = **input_str;
 			if (**input_str)
 				(*input_str)++;
 		}
-		if (set_mode_status == -1 || set_mode_status == 1)
-			return (set_mode_status);
+		if (set_mode_status == -1 || set_mode_status == PIPE)
+			break ;
 	}
 	el->str_to_cmd[i] = '\0';
-	if (!shell)
-		return (0);
 	el->cmd_arr = parse_args(el->str_to_cmd, shell);
-	return (0);
+	return (set_mode_status);
 }
 
 int	parser(char *input_str, t_list **list, t_mshell *shell)
@@ -171,6 +173,8 @@ int	parser(char *input_str, t_list **list, t_mshell *shell)
 	{
 		el = create_empty_el();
 		el->str_to_cmd = malloc(ft_strlen(input_str) + 1);
+		if (!el->str_to_cmd)
+			return (-1);
 		if (set_params_to_el(&input_str, el, shell) == -1)
 		{
 			printf("Okay, Houston...we've had a problem here. Moon is "
@@ -185,7 +189,7 @@ int	parser(char *input_str, t_list **list, t_mshell *shell)
 /*TODO for pipe one more handler in valid for extra pipe*/
 /*TODO 'cats  "\"c\'at\"  >lol\'"  -->>str_to_cmd: |"c'at"  | output_file: |lol'| */
 /*TODO validator has intput-output-||| */
-/*some sega in parse env MAKEFLAGS=?????!!!!?*/
+/*TODO !!!!!!!!!!!!!!!!!!!!!!"cat some > out > out2 */
 int	main(int argc, char **argv, char **envp)
 {
 	t_list			*list;
@@ -194,10 +198,9 @@ int	main(int argc, char **argv, char **envp)
 	(void)argc;
 	(void)argv;
 ////////
-	char *s = "cat input> output ";
+	char *s = "cat lol > lol2 > lol3 > lol4";
 	list = NULL;
 	init_shell(&shell, envp);
-
 	parser(s, &list, &shell);
 	validation(list, envp);
 	show_params(list);
